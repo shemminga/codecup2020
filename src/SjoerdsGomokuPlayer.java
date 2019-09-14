472807c0109a353da4926b77b1daff7b75013efd
Sjoerd Hemminga <sjoerd@hemminga-online.nl>
2019-09-15 00:11:38 +0200

    Inject dependencies, so we can have players play against each other.

--- src/SjoerdsGomokuPlayer.java
+++ src/SjoerdsGomokuPlayer.java
@@ -1,158 +1,96 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Random;
 
 public class SjoerdsGomokuPlayer {
     private static final long START_UP_TIME = System.nanoTime();
-    private static final PrintTool PRINTER = new PrintTool();
-    private static final Random rnd = new Random();
 
-    static {
+    private final DbgPrinter dbgPrinter;
+    private final Random rnd;
+    private final IO io;
+
+    public static void main(String[] args) throws IOException {
+        final DbgPrinter dbgPrinter = new DbgPrinter(System.err);
+
         @SuppressWarnings("NumericOverflow")
         final long seed = 8682522807148012L * 1181783497276652981L * System.nanoTime();
-        rnd.setSeed(seed);
-        log("SD " + seed);
+        dbgPrinter.log("SD " + seed);
+
+        final Random rnd = new Random(seed);
+        final IO io = new IO(new MoveConverter(dbgPrinter), System.in, System.out, dbgPrinter);
+        final SjoerdsGomokuPlayer player = new SjoerdsGomokuPlayer(rnd, io, dbgPrinter);
+
+        player.play();
     }
 
-    private static void log(String message) {
-        System.err.println((System.nanoTime() - START_UP_TIME) + " " + message);
-    }
-
-    public static void main(String[] args) {
-        try {
-            main();
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(0); // Exit nicely, cause why not?
-        }
+    private SjoerdsGomokuPlayer(final Random rnd, final IO io, final DbgPrinter dbgPrinter) {
+        this.rnd = rnd;
+        this.io = io;
+        this.dbgPrinter = dbgPrinter;
     }
 
     @SuppressWarnings("InfiniteLoopStatement")
-    private static void main() throws IOException {
+    private void play() throws IOException {
         final Board board = new Board();
 
-        final Move firstMove = IO.readMove();
+        final Move firstMove = io.readMove();
 
         if (firstMove == Move.START) {
             for (int i = 0; i < 3; i++) {
                 final Move move = generateMove(board);
-                board.apply(move);
-                IO.outputMove(move);
+                applyMove(board, move);
+                io.outputMove(move);
             }
         } else {
             board.apply(firstMove);
 
             for (int i = 0; i < 2; i++) {
-                Move move = IO.readMove();
-                board.apply(move);
+                Move move = io.readMove();
+                applyMove(board, move);
             }
 
             if (rnd.nextBoolean()) {
                 board.flip();
-                IO.outputMove(Move.SWITCH);
+                io.outputMove(Move.SWITCH);
             } else {
                 final Move move = generateMove(board);
-                board.apply(move);
-                IO.outputMove(move);
+                applyMove(board, move);
+                io.outputMove(move);
             }
         }
 
         while (true) {
-            Move move = IO.readMove();
-            board.apply(move);
+            Move move = io.readMove();
+            applyMove(board, move);
 
             final Move myMove = generateMove(board);
             board.apply(myMove);
-            IO.outputMove(myMove);
+            io.outputMove(myMove);
         }
     }
 
-    private static Move generateMove(Board board) {
+    private void applyMove(Board board, Move move) {
+        board.apply(move);
+        dbgPrinter.printBoard("GB", board);
+    }
+
+    private Move generateMove(Board board) {
         return rnd.ints(0, 256)
-                .mapToObj(Move::fromFieldIdx)
+                .mapToObj(io.moveConverter::toMove)
                 .filter(board::validMove)
                 .findAny()
                 .orElseThrow();
     }
 
-    private static final class IO {
-        private static Move readMove() throws IOException {
-            System.err.println('-');
-            final int rowInt = robustRead();
-            final int colInt = robustRead();
+    private static final class MoveConverter {
+        private final DbgPrinter dbgPrinter;
 
-            final int fieldIdx = (rowInt - 'A') * 16 + (colInt - 'a');
-            String moveStr = (char) rowInt + "" + (char) colInt;
-
-            if (fieldIdx == 307) {
-                // = "St", first letters of Start
-                System.in.skip(3);
-                PRINTER.printMove("IM", moveStr, Move.START);
-                return Move.START;
-            }
-
-            if (fieldIdx == 276) {
-                // = "Qu", first letters of Quit
-                PRINTER.printMove("IM", moveStr, Move.QUIT);
-                log("Exit by command");
-                System.exit(0);
-            }
-
-            final Move move = Move.fromFieldIdx(fieldIdx);
-
-            PRINTER.printMove("IM", moveStr, move);
-            return move;
+        private MoveConverter(DbgPrinter dbgPrinter) {
+            this.dbgPrinter = dbgPrinter;
         }
 
-        private static int robustRead() throws IOException {
-            int val;
-
-            do {
-                val = System.in.read();
-                if (val < 0) {
-                    // End of game
-                    System.exit(0);
-                }
-            } while (val == 32 || val == 9 || val == 10 ||
-                    val == 13); // Skip some whitespace, to help with buffered input
-
-            return val;
-        }
-
-        private static void outputMove(Move move) {
-            final String moveStr;
-            if (move == Move.SWITCH) {
-                moveStr = "Zz";
-            } else {
-                int fieldIdx = move.toFieldIdx();
-
-                final int col = fieldIdx % 16;
-                final int row = (fieldIdx - col) / 16;
-
-                final char rowC = (char) (row + 'A');
-                final char colC = (char) (col + 'a');
-                moveStr = "" + rowC + colC;
-            }
-
-            PRINTER.printMove("OM", moveStr, move);
-
-            System.err.flush(); // Flush stderr so debug output and regular output are ordered correctly
-            System.out.println(moveStr);
-            System.out.flush();
-        }
-    }
-
-    private static final class Move {
-        private static final Move START = new Move(new long[]{0, 0, 0, 0});
-        private static final Move QUIT = new Move(new long[]{0, 0, 0, 0});
-        private static final Move SWITCH = new Move(new long[]{0, 0, 0, 0});
-
-        private long[] move;
-
-        private Move(final long[] move) {
-            this.move = move;
-        }
-
-        private static Move fromFieldIdx(final int fieldIdx) {
+        private Move toMove(int fieldIdx) {
             long[] moveLong;
             if (fieldIdx < 64) {
                 moveLong = new long[]{Long.MIN_VALUE >>> fieldIdx, 0, 0, 0};
@@ -164,17 +102,18 @@ public class SjoerdsGomokuPlayer {
                 moveLong = new long[]{0, 0, 0, Long.MIN_VALUE >>> (fieldIdx - 192)};
             } else if (fieldIdx == 425) {
                 // "Zz", meaning the other side lets us play
-                return SWITCH;
+                return Move.SWITCH;
             } else {
-                log("Unexpected move fieldIdx " + fieldIdx);
+                dbgPrinter.log("Unexpected move fieldIdx " + fieldIdx);
                 moveLong = new long[]{0, 0, 0, 0};
             }
 
             return new Move(moveLong);
         }
 
-        private int toFieldIdx() {
-            return findBit(move[0], 0) + findBit(move[1], 64) + findBit(move[2], 128) + findBit(move[3], 192);
+        private int toFieldIdx(Move move) {
+            return findBit(move.move[0], 0) + findBit(move.move[1], 64) + findBit(move.move[2], 128) +
+                    findBit(move.move[3], 192);
         }
 
         private static int findBit(final long l, final int offset) {
@@ -189,6 +128,96 @@ public class SjoerdsGomokuPlayer {
             }
 
             return 0;
+        }
+    }
+
+    private static final class IO {
+        private final MoveConverter moveConverter;
+        private final InputStream in;
+        private final PrintStream out;
+        private final DbgPrinter dbgPrinter;
+
+        private IO(MoveConverter moveConverter, InputStream in, PrintStream out, final DbgPrinter dbgPrinter) {
+            this.moveConverter = moveConverter;
+            this.in = in;
+            this.out = out;
+            this.dbgPrinter = dbgPrinter;
+        }
+
+        private Move readMove() throws IOException {
+            dbgPrinter.separator();
+            final int rowInt = robustRead();
+            final int colInt = robustRead();
+
+            final int fieldIdx = (rowInt - 'A') * 16 + (colInt - 'a');
+            String moveStr = (char) rowInt + "" + (char) colInt;
+
+            if (fieldIdx == 307) {
+                // = "St", first letters of Start
+                in.skip(3);
+                dbgPrinter.printMove("IM", moveStr, Move.START);
+                return Move.START;
+            }
+
+            if (fieldIdx == 276) {
+                // = "Qu", first letters of Quit
+                dbgPrinter.printMove("IM", moveStr, Move.QUIT);
+                dbgPrinter.log("Exit by command");
+                System.exit(0);
+            }
+
+            final Move move = moveConverter.toMove(fieldIdx);
+
+            dbgPrinter.printMove("IM", moveStr, move);
+            return move;
+        }
+
+        private int robustRead() throws IOException {
+            int val;
+
+            do {
+                val = in.read();
+                if (val < 0) {
+                    // End of game
+                    System.exit(0);
+                }
+            } while (val == 32 || val == 9 || val == 10 || val == 13); // Whitespace
+
+            return val;
+        }
+
+        private void outputMove(Move move) {
+            final String moveStr;
+            if (move == Move.SWITCH) {
+                moveStr = "Zz";
+            } else {
+                int fieldIdx = moveConverter.toFieldIdx(move);
+
+                final int col = fieldIdx % 16;
+                final int row = (fieldIdx - col) / 16;
+
+                final char rowC = (char) (row + 'A');
+                final char colC = (char) (col + 'a');
+                moveStr = "" + rowC + colC;
+            }
+
+            dbgPrinter.printMove("OM", moveStr, move);
+
+            dbgPrinter.flush(); // Flush debug output so debug and regular output are ordered correctly
+            out.println(moveStr);
+            out.flush();
+        }
+    }
+
+    private static final class Move {
+        private static final Move START = new Move(new long[]{0, 0, 0, 0});
+        private static final Move QUIT = new Move(new long[]{0, 0, 0, 0});
+        private static final Move SWITCH = new Move(new long[]{0, 0, 0, 0});
+
+        private long[] move;
+
+        private Move(final long[] move) {
+            this.move = move;
         }
     }
 
@@ -213,7 +242,6 @@ public class SjoerdsGomokuPlayer {
 
             playerToMove = ~playerToMove;
 
-            PRINTER.printBoard("GB", this);
             return this;
         }
 
@@ -221,8 +249,6 @@ public class SjoerdsGomokuPlayer {
             long[] swap = playerPieces;
             playerPieces = opponentPieces;
             opponentPieces = swap;
-
-            PRINTER.printBoard("GB", this);
             return this;
         }
 
@@ -247,10 +273,16 @@ public class SjoerdsGomokuPlayer {
         }
     }
 
-    public static class PrintTool {
+    private static class DbgPrinter {
+        private final PrintStream err;
+
+        private DbgPrinter(final PrintStream err) {
+            this.err = err;
+        }
+
         void printBoard(String type, Board board) {
             log(type + " " +
-                    (board.playerToMove == Board.PLAYER ? "P" : "O") + " " +
+                    (board.playerToMove == Board.PLAYER ? "Pl" : "Op") + " " +
                     Long.toHexString(board.playerPieces[0]) + " " +
                     Long.toHexString(board.playerPieces[1]) + " " +
                     Long.toHexString(board.playerPieces[2]) + " " +
@@ -273,6 +305,18 @@ public class SjoerdsGomokuPlayer {
                 log(type + " " + moveStr + " " + Long.toHexString(move.move[0]) + " " + Long.toHexString(move.move[1]) +
                         " " + Long.toHexString(move.move[2]) + " " + Long.toHexString(move.move[3]));
             }
+        }
+
+        private void log(String message) {
+            err.println((System.nanoTime() - START_UP_TIME) + " " + message);
+        }
+
+        private void separator() {
+            err.println('-');
+        }
+
+        private void flush() {
+            err.flush();
         }
     }
 }
